7ac07a77fde7f6d51e9cd511f711f601d48f6bb6
xanass69 <himdo368@gmail.com>
2026-04-20 16:26:06 +0100

    Initial commit: FireStorm reverse engineering lab

---
+++ frida_firestorm.js
@@ -0,0 +1,24 @@
+﻿Java.perform(function() {
+
+    function getPassword() {
+        console.log("[*] Recherche des instances de MainActivity...");
+
+        Java.choose('com.pwnsec.firestorm.MainActivity', {
+            onMatch: function(instance) {
+                console.log("[+] Instance trouvée : " + instance);
+                try {
+                    var pass = instance.Password();
+                    console.log("[+] Mot de passe Firebase : " + pass);
+                } catch (e) {
+                    console.log("[-] Erreur : " + e);
+                }
+            },
+            onComplete: function() {
+                console.log("[*] Recherche terminée.");
+            }
+        });
+    }
+
+    console.log("[*] Script chargé. Attente 3 secondes...");
+    setTimeout(getPassword, 3000);
+});
---
+++ get_flag.py
@@ -0,0 +1,25 @@
+﻿import pyrebase
+
+config = {
+    "apiKey": "AIzaSyAXsK0qsx4RuLSA9C8IPSWd0eQ67HVHuJY",
+    "authDomain": "firestorm-9d3db.firebaseapp.com",
+    "databaseURL": "https://firestorm-9d3db-default-rtdb.firebaseio.com",
+    "storageBucket": "firestorm-9d3db.appspot.com",
+    "projectId": "firestorm-9d3db"
+}
+
+firebase = pyrebase.initialize_app(config)
+auth = firebase.auth()
+
+email = "TK757567@pwnsec.xyz"
+password = "C7_dotpsC7t7f_._In_i.IdttpaofoaIIdIdnndIfC"
+
+print("[*] Connexion à Firebase...")
+user = auth.sign_in_with_email_and_password(email, password)
+print("[+] Connexion réussie !")
+
+db = firebase.database()
+flag_data = db.get(user['idToken'])
+print("\n" + "="*50)
+print("FLAG :", flag_data.val())
+print("="*50)
